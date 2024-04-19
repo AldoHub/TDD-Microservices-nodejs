c43da0abd09cc4a2ae939caeee369b29802bfec8
aldocaava <aldocaava@hotmail.com>
2024-04-19 00:29:23 -0500

    added Prisma ORM and Docker postgresql

--- catalog-service/src/repository/catalog.repository.ts
+++ catalog-service/src/repository/catalog.repository.ts
@@ -1,21 +1,46 @@
+import { PrismaClient } from "@prisma/client";
 import { ICatalogRepository } from "../interface/catalogRepository.interface";
 import { Product } from "../models/product.model";
 
 export class CatalogRepository implements ICatalogRepository {
+
+    _prisma: PrismaClient
+
+    constructor(){
+        this._prisma = new PrismaClient();
+    }
+
+
     create(data: Product): Promise<Product> {
-        throw new Error("Method not implemented.");
+        return this._prisma.product.create({data});
     }
     update(data: Product): Promise<Product> {
-        throw new Error("Method not implemented.");
+       return this._prisma.product.update({
+        where: {id: data.id},
+        data
+       });
     }
     delete(id: number): Promise<Product> {
-        throw new Error("Method not implemented.");
+        return this._prisma.product.delete({
+            where: {id},
+        });
     }
-    findAll(): Promise<Product[]> {
-        throw new Error("Method not implemented.");
+    findAll(limit: number, offset: number): Promise<Product[]> {
+       return this._prisma.product.findMany({
+        take: limit,
+        skip: offset
+       });
     }
-    findOne(id: number): Promise<Product> {
-        throw new Error("Method not implemented.");
+    async findOne(id: number): Promise<Product> {
+       const product= await this._prisma.product.findFirst({
+        where: {id},
+       })
+
+       if(product){
+            return Promise.resolve(product);
+       }
+
+       throw new Error("Product not found");
     }
    
     
